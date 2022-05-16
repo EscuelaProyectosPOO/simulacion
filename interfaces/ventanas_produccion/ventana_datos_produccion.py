2b33b141c975f49e53772ca21f312d03c85b731f
Estefania <tetillamas@gmail.com>
2022-05-15 19:36:03 -0500

    creacion de interfaces y conexion a ventana de produccion

---
+++ interfaces/ventanas_produccion/ventana_datos_produccion.py
@@ -0,0 +1,34 @@
+import re
+import sys
+from PyQt5.QtWidgets import QDialog, QMessageBox, QApplication
+from PyQt5 import uic
+from PyQt5 import QtWidgets
+import ctypes #getSystemMetr
+
+class Ventana_datos_ventas(QDialog):
+
+    def __init__(self):
+
+        QDialog.__init__(self)
+        #super().__init__()
+        #cargar la configuracion del archivo .ui en el objeto
+        uic.loadUi("interfaces/ventanas_produccion/Ingreso_datos_produccion.ui", self)
+
+        #centrar ventana
+        resolucion = ctypes.windll.user32
+        resolucion_ancho = resolucion.GetSystemMetrics(0)
+        resolucion_alto = resolucion.GetSystemMetrics(1)
+
+        left = int((resolucion_ancho/2) - (self.frameSize().width()/2))
+        top = int((resolucion_alto/2) - (self.frameSize().height()/2))
+
+        self.move(left,top)
+
+if(__name__ == "__main__"):
+
+    #Instancia para iniciar la aplicsacion
+    app = QApplication(sys.argv)
+    ventana = Ventana_datos_ventas()
+    ventana.show()
+    #ejecutar la aplicacion
+    app.exec_()
